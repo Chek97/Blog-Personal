30e9d18583182132674b9a960947a6d8122d3f43
Chek97 <felychk203@hotmail.com>
2021-06-05 00:06:19 -0500

    terminadas las paginas de entrada y comentarios, falta la de estadisticas

--- view/Main/commentsPage.php
+++ view/Main/commentsPage.php
@@ -7,35 +7,73 @@
 </head>
 
 <body>
-    <?php require_once('./navbar.php'); ?>
+    <?php
+        require_once('./navbar.php');
+        require_once('../../controller/Comment/commentsController.php');
+        $objCom = new CommentActions();
+    ?>
     <div class="container">
         <header>
             <h1>COMENTARIOS</h1>
         </header>
-        <?php 
-            //si no hay entradas colocar alerta que no hay cartas
+        <?php
+            if (isset($_GET['q'])) {
+                $listComments = $objCom->getComments($_GET['q']);
+                if ($listComments == false) { 
         ?>
-        <div class="alert alert-message m-5 pt-5 pb-5" role="alert">
-            <strong>No hay comentarios todavia</strong>
+                    <div class="alert alert-message m-5 pt-5 pb-5" role="alert">
+                        <strong>No hay comentarios todavia</strong>
+                    </div>
+        <?php   }else{
+                    foreach ($listComments as $comment) { 
+        ?>
+                        <div class="m-3">
+                            <div class="card mb-5">
+                                <div class="card-header">
+                                    <?php echo($comment['autor_id']); ?> ha comentado en <?php echo($comment['entrada_id']); ?>
+                                </div>
+                                <div class="card-body">
+                                    <blockquote class="blockquote mb-0">
+                                        <p><?php echo($comment['valor']); ?></p>
+                                        <footer class="blockquote-footer">Comentado el<cite title="Source Title"> <?php echo($comment['fecha']); ?><!-- 16 abr 2021 --></cite></footer>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+            <?php   }
+                }
+            } else {
+                $allComments = $objCom->getComments();
+                if ($allComments == false) { 
+        ?>
+                    <div class="alert alert-message m-5 pt-5 pb-5" role="alert">
+                        <strong>No hay comentarios todavia</strong>
+                    </div>
+        <?php   }else{
+                    foreach ($allComments as $comment) { 
+        ?>
+                        <div class="m-3">
+                            <div class="card mb-5">
+                                <div class="card-header">
+                                    <?php echo($comment['autor_id']); ?> ha comentado en <?php echo($comment['entrada_id']); ?>
+                                </div>
+                                <div class="card-body">
+                                    <blockquote class="blockquote mb-0">
+                                        <p><?php echo($comment['valor']); ?></p>
+                                        <footer class="blockquote-footer">Comentado el<cite title="Source Title"> <?php echo($comment['fecha']); ?><!-- 16 abr 2021 --></cite></footer>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+        <?php       }
+                } 
+            }
+        ?>
         </div>
-        <div class="m-3">
-            <div class="card mb-5">
-                <div class="card-header">
-                    {{Author}} ha comentado en {{nombreEntrada}}
-                </div>
-                <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <p>A well-known quote, contained in a blockquote element.</p>
-                        <footer class="blockquote-footer">Comentado el<cite title="Source Title"> 16 abr 2021</cite></footer>
-                    </blockquote>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php
-    include('./foot.php');
-    require_once('../footer.php');
-    ?>
+        <?php
+            include('./foot.php');
+            require_once('../footer.php');
+        ?>
 </body>
 
 </html>
